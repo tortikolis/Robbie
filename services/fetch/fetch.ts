24ada0d774584433384bebe19e49722c8ad1f384
Jovica <jovicabit@gmail.com>
2018-10-22 16:14:02 +0200

    change fetch

--- services/fetch/fetch.ts
+++ services/fetch/fetch.ts
@@ -1,7 +1,8 @@
 module.exports = (url: string) => {
   const fetch = require('node-fetch');
+  const Joke = require('../../entities/joke');
   
   return fetch(url)
   .then((res) => res.json())
-  .then((data) => data)
+  .then((data) => new Joke(data.value.joke))
 }
