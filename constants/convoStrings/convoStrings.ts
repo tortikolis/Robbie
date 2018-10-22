d552b73af632feff6a599751b55188147ad98523
Jovica <jovicabit@gmail.com>
2018-10-22 20:01:28 +0200

    change initial convo

--- constants/convoStrings/convoStrings.ts
+++ constants/convoStrings/convoStrings.ts
@@ -3,7 +3,7 @@ const convoStrings: object = {
   ['Hi there, let me introduce myself. My name is Robbie and I am funny bot!', 
   'I can tell you joke or prank, so wich one do you want?'],
 
-  initialWelcome: 'Hello! My name is Robbie and I can make you laugh. Say hello to start conversation',
+  initialWelcome: 'Hello! My name is Robbie and I can make you laugh. Say hello to start conversation...',
 
   jokeConvo: 'Sure! Do you want random joke or specific one from some of categories:',
 
